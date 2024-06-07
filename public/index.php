945d62e82e6dfa0aa9c6ba1e03af6cdfef812676
joaogmendes <jonathammises@proton.me>
2024-06-07 13:07:14 -0300

    refactor: Separate Controller Logic into New File

--- public/index.php
+++ public/index.php
@@ -2,28 +2,8 @@
 // Autoload
 require_once __DIR__ . '/../vendor/autoload.php';
 
-// Imports
-use Alice\Animeland\Core\Ralts;
-use Alice\Animeland\Controller\HomeController;  // Home Controller
-use Alice\Animeland\Controller\SignupController;
-use Alice\Animeland\Form\SignupHandler;
-use Alice\Animeland\Controller\LoginController;
-use Alice\Animeland\Form\Authenticate;
-use Alice\Animeland\Controller\TestRouter;
-
 // Migration
 require '../src/Migration.php';
 
 // Routes
-Ralts::get('/animes', fn() => HomeController::index());
-Ralts::get('/signup', fn() => SignupController::index());
-Ralts::get('/login', fn() => LoginController::index());
-
-// Form Handles
-Ralts::post('/signup', fn() => SignupHandler::index());
-Ralts::post('/login', fn() => Authenticate::index());
-
-Ralts::get('/teste/(:num)', fn($id) => TestRouter::index($id));
-
-
-Ralts::dispatch();
+require '../src/Controller/Controller.php';
---
+++ src/Controller/Controller.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Alice\Animeland\Controller;
+
+use Alice\Animeland\Core\Ralts;
+use Alice\Animeland\Controller\HomeController;
+use Alice\Animeland\Controller\SignupController;
+use Alice\Animeland\Form\SignupHandler;
+use Alice\Animeland\Controller\LoginController;
+use Alice\Animeland\Form\Authenticate;
+use Alice\Animeland\Controller\TestRouter;
+
+
+Ralts::get('/animes', fn() => HomeController::index());
+Ralts::get('/signup', fn() => SignupController::index());
+Ralts::get('/login', fn() => LoginController::index());
+Ralts::get('/teste/(:num)', fn($id) => TestRouter::index($id));
+
+// Form Handles
+Ralts::post('/signup', fn() => SignupHandler::index());
+Ralts::post('/login', fn() => Authenticate::index());
+
+
+Ralts::dispatch();
