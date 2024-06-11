d936ed6f97effa514dfe93e6df149c991f387522
joaogmendes <jonathammises@proton.me>
2024-06-11 15:25:25 -0300

    Updating the home view to adapt to a new anime model

--- src/View/Home.php
+++ src/View/Home.php
@@ -6,34 +6,29 @@ $content = <<<EOD
 EOD;
 
 foreach($animes as $anime) {
- $content .= <<<EOD
+  $content .= <<<EOD
+<a href="/anime/$anime[mal_id]">
 <div class="column is-8"clas="card"  style="width: 220px">
   <div class="card"  style="width: 200px">
-  <div style="text-align: center; height: 50px; overflow: hidden" class="p-1">
-    <div>
-        <p class="title is-6"> $anime[title]</p>
-        <p class="subtitle is-6"></p>
-      </div>
-  </div>
-
+ 
   <div class="card">
     <div class="card-image">
       <figure class="is-4by3">
-        <img src="https://cdn.myanimelist.net/images/anime/1224/113092.jpg"/>
+        <img src="$anime[cover_url]"/>
       </figure>
     </div>
   </div>
 
-  <div>
-    <div class="p-2" class="content" style="text-align: center;"> 
-      <a href="#">#Adventure</a>
-      <a href="#">#Suspense</a>
-      <br />
-      <time datetime="2016-1-1"> - $anime[release_date]</time>
-    </div>
+  <div style="text-align: center; height: 50px; overflow: hidden"  class="p-1">
+      <div>
+          <p class="title is-6"> $anime[title]</p>
+      </div>
+  </div>
+
+
   </div>
   </div>
-</div>
+</a>
 EOD;
 }
 
